de998deff2e814f675f718655a31148048b4404c
Jesus Lorca Luque <jeslorlu@gmail.com>
2019-08-21 21:48:22 +0200

    add book class

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book {
+        private List<double> _grades;
+        public List<double> Grades 
+        { 
+            get
+            {
+                if(_grades == null)
+                {
+                    this._grades = new List<double>();
+                }
+
+                return this._grades;
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            this.Grades.Add(grade);
+        }
+
+        public double Average()
+        {
+            int numGrades = this.Grades.Count;
+            if(numGrades == 0) {
+                return 0;
+            }
+            double sumGrades = 0;
+            foreach (var grade in this.Grades)
+            {
+                sumGrades += grade;
+            }
+            return sumGrades/numGrades;
+        }
+
+        public double MaxGrade()
+        {
+            double maxGrade = double.MinValue;
+            foreach (var grade in this.Grades)
+            {
+                maxGrade = Math.Max(grade, maxGrade);
+            }
+
+            return maxGrade;
+        }
+
+        public double MinGrade()
+        {
+            double minGrade = double.MaxValue;
+            foreach (var grade in this.Grades)
+            {
+                minGrade = Math.Min(grade, minGrade);
+            }
+
+            return minGrade;
+        }
+
+        public string ToString()
+        {
+            string result = "";
+            foreach (var grade in Grades)
+            {
+                result += grade + " ";
+            }
+
+            return result;
+        }
+    }
+}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int> {1, 4};
+            System.Console.WriteLine("Size = " + numbers.Count);
+            System.Console.WriteLine("numbers = " + numbers[1]);
+            Book b = new Book();
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}");
@@ -14,6 +19,15 @@ namespace GradeBook
             {
                 Console.WriteLine("Nothing!");
             }
+
+            Book book1 = new Book();
+            book1.AddGrade(5.8);
+            book1.AddGrade(8.5);
+            book1.AddGrade(4.2);
+            System.Console.WriteLine("Average = " + book1.Average());
+            System.Console.WriteLine("MaxGrade = " + book1.MaxGrade());
+            System.Console.WriteLine("MinGrade = " + book1.MinGrade());
+            System.Console.WriteLine("Book1 = " + book1.ToString());
         }
     }
 }
